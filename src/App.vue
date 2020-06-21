afe7e56186d8580f512b66201d5cdd7d13c7dfa6
Pankaj Sharma <pankaj.sharma1@squareyards.co.in>
2020-06-21 20:33:16 +0530

    - Added ability to delete completed - Shows a message if no incomplete tasks remain

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,23 @@
       {{name}}'s To Do list
     </h4>
     <div class="container-fluid p-4">
-      <div class="row">
-        <div class="col font-weight-bold">Task</div>
-        <div class="col-2 font-weight-bold">Done</div>
-      </div>
-      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
-        <div class="col">{{t.action}}</div>
-        <div class="col-2 text-center">
-          <input type="checkbox" v-model="t.done" class="form-check-input">
+      <div class="row" v-if="filteredTasks.length == 0">
+        <div class="col text-center">
+          <b>Nothing to do. Hurrah!</b>
         </div>
       </div>
+      <template v-else>
+        <div class="row">
+          <div class="col font-weight-bold">Task</div>
+          <div class="col-2 font-weight-bold">Done</div>
+        </div>
+        <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+          <div class="col">{{t.action}}</div>
+          <div class="col-2 text-center">
+            <input type="checkbox" v-model="t.done" class="form-check-input">
+          </div>
+        </div>
+      </template>
       <div class="row py-2">
         <div class="col">
           <input v-model="newItemText" type="text" class="form-control">
@@ -28,6 +35,11 @@
           <label class="form-check-label font-weight-bold">
             Hide completed tasks
           </label>
+        </div>
+        <div class="col text-center">
+          <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">
+            Delete Completed
+          </button>
         </div>
       </div>
     </div>
@@ -57,8 +69,15 @@ export default {
         action: this.newItemText,
         done: false
       });
-      localStorage.setItem("todos", JSON.stringify(this.tasks));
+      this.storeData();
       this.newItemText = "";
+    },
+    deleteCompleted() {
+      this.tasks = this.tasks.filter(t => !t.done);
+      this.storeData();
+    },
+    storeData() {
+      localStorage.setItem("todos", JSON.stringify(this.tasks));
     }
   },
   created() {
